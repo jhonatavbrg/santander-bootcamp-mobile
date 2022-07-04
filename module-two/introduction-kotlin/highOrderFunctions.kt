e21ce9e7b8727a4eb1432da1489cfa0b371f81fb
Jhonata Braga <jhonatavbrg@gmail.com>
2022-07-04 16:35:13 -0300

    add exercises

---
+++ module-two/introduction-kotlin/highOrderFunctions.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val x:Int
+    val z:Int
+
+    z = calculate(34, 90, ::sum)
+    println(z)
+}
+
+fun sum(a1:Int, a2:Int) = a1.plus(a2)
+
+fun calculate(n1:Int, n2:Int, operation:(Int,Int) -> Int):Int {
+    val result = operation(n1, n2)
+    return result
+}
---
+++ module-two/introduction-kotlin/logicalOperators.kt
@@ -0,0 +1,9 @@
+const val MIN_AGE = 16
+const val MAX_AGE = 68
+
+fun main() {
+    var age = (10..100).random() 
+    println(age)
+    println(age in MIN_AGE..MAX_AGE)
+    println(age >= MIN_AGE && age <= MAX_AGE)
+}
